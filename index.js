5ba85fc6535c9dadcfb95ead29a6422ebe11e2f6
Desh Bagley <stemtrainer@gmail.com>
2022-03-27 22:24:53 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+// Write your solution in this fill
+const employee = {}
+employee.name = "Desh"
+employee.streetAddress = "Climbing Fern"
+
+//employee
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee, [key]:value}
+}
+
+//creates an object clone and adds an key:value
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+     employee[key] = value
+     return employee
+}
+
+//delete a key from an object and save a clone
+function deleteFromEmployeeByKey(employee, key){
+    let clone = {...employee}
+    delete clone[key];
+    return clone
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
+
+
+
+//updateEmployeeWithKeyAndValue(employee, "score", 100)
+//destructivelyUpdateEmployeeWithKeyAndValue(employee, "score", 100)
+deleteFromEmployeeByKey(employee, "name")  
